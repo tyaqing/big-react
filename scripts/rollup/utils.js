288511815a30fe206d5977bbe88bfcf515c4c21e
yakirteng <yakirteng@tencent.com>
2023-01-13 23:44:57 +0800

    feat: 修改eslint问题

--- scripts/rollup/utils.js
+++ scripts/rollup/utils.js
@@ -3,28 +3,31 @@ import fs from 'fs';
 
 import ts from 'rollup-plugin-typescript2';
 import cjs from '@rollup/plugin-commonjs';
-import from '@rollup/plugin-replace';
+import replace from '@rollup/plugin-replace';
 
 const pkgPath = path.resolve(__dirname, '../../packages');
 const distPath = path.resolve(__dirname, '../../dist/node_modules');
 
 export function resolvePkgPath(pkgName, isDist) {
-  if (isDist) {
-    return `${distPath}/${pkgName}`;
-  }
-  return `${pkgPath}/${pkgName}`;
+	if (isDist) {
+		return `${distPath}/${pkgName}`;
+	}
+	return `${pkgPath}/${pkgName}`;
 }
 
 export function getPackageJSON(pkgName) {
-  const path = `${resolvePkgPath(pkgName)}/package.json`;
-  const str = fs.readFileSync(path, {encoding: 'utf-8'});
-  return JSON.parse(str);
+	const path = `${resolvePkgPath(pkgName)}/package.json`;
+	const str = fs.readFileSync(path, { encoding: 'utf-8' });
+	return JSON.parse(str);
 }
 
 export function getBaseRollupPlugins({
- alias = {
-   __LOG__: false, preventAssignment: true, __DEV__: true
- }, typescript = {}
+	alias = {
+		__LOG__: false,
+		preventAssignment: true,
+		__DEV__: true
+	},
+	typescript = {}
 } = {}) {
-  return [replace(alias), cjs(), ts(typescript)];
+	return [replace(alias), cjs(), ts(typescript)];
 }
